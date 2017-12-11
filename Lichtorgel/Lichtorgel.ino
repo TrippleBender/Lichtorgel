fd21a381efa023c52714806a76e9d491a0a26f6b
TrippleBender <bennide03@gmx.de>
2017-12-11 23:24:39 +0100

    Blinkanzeige hinzugefügt und Parameter etwas angepasst

--- Lichtorgel/Lichtorgel.ino
+++ Lichtorgel/Lichtorgel.ino
@@ -5,15 +5,21 @@ Adafruit Microphone Amplifier
 
 unsigned int led = 3;                   // PWM Output Pin for LED
 
-double amplify = 10.0;
+double amplify = 7.0;
 double brightness = 0.0;
-int peakToPeak = 0;            // peak-to-peak level
+int peakToPeak = 0;
 unsigned int i = 0;
 unsigned int j = 0;
+
+unsigned long lagMillis = 0;
+unsigned long outTimeMillis = 0;
+unsigned int amplifyOld = 0;
+unsigned int k = 0;
 
 void setup() 
 {  
   pinMode(led, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -27,15 +33,19 @@ void loop()
   const double pwm = 255.0;
   const double pwmMin = 50.0;
   
-  const double amplifyMax = 10.0;          // Regulater
+  const double amplifyMax = 7.0;          // Regulater
   const double amplifyMin = 1.0;
   const double difference = 1.0;
-  const unsigned int limit = 9;
+  const unsigned int limit = 5;           // Wie oft in die Begrenzung gefahren werden darf
 
   const int sensitvity = 15;    // Filter
   int oldPTP = 0;
 
-  unsigned long startMillis= millis();    // Start of sample window
+  unsigned int lag = 250;                  // For the Builtin_led to show the Amplify
+  unsigned int outTime = 1000;
+
+
+  unsigned long startMillis = millis();    // Start of sample window
 
   unsigned int signalMax = 0;
   unsigned int signalMin = micMax;
@@ -61,51 +71,48 @@ void loop()
   }
   peakToPeak = signalMax - signalMin;    // max - min = peak-peak amplitude
   //Serial.println(peakToPeak);
-  Serial.println(oldPTP-peakToPeak);
+  //Serial.println(oldPTP-peakToPeak);
 
   // Filter
   if(sensitvity < oldPTP - peakToPeak || -sensitvity > oldPTP - peakToPeak)
   {
-    const unsigned int fadeAmount = 10;
-    const unsigned int regulateWindow = 20;
+    const unsigned int fadeAmount = 5;
+    const unsigned int regulateWindow = 15;
     double target = amplify * (peakToPeak * pwm) / micMax;  // convert to pwm
-    j = 0;
+    amplifyOld = (int) amplify;
+
 
     // Limiter
     if(target >= pwm)
     {
-      target = pwm;
+      target = pwm;                    // set maximal brightness
       i = i + 1;
       j = 0;
-      Serial.print("i");
-      Serial.println(i);
     }
-    else if(target <= pwmMin)
+    else if(target <= pwmMin)          // set minimal brightness
     {
       target = pwmMin;
       j = j + 1;
       i = 0;
-      Serial.print("j");
-      Serial.println(j);
     }
 
     // Regulater
-    if(i > limit && amplify > amplifyMin)
+    if(i > limit && amplify > amplifyMin)         // reduces the Amplify if overload
     {
-      amplify = amplify - difference;
+      amplify -= difference;
       i = 0;
       j = 0;
     }
-    else if(j > limit && amplify < amplifyMax)
+    else if(j > limit && amplify < amplifyMax)    // raise the Amplify if always minimal brightness is used 
     {
-      amplify = amplify + difference;
+      amplify += difference;
       j = 0;
       i = 0;
     }
       
     startMillis = millis();
 
-    while(brightness < target-fadeAmount || brightness > target+fadeAmount || millis() - startMillis < regulateWindow)
+    while(brightness < target-fadeAmount || brightness > target+fadeAmount || millis() - startMillis < regulateWindow)          // fading the brightness
     {
       if(brightness > target)
       {
@@ -115,57 +122,47 @@ void loop()
       {
         brightness += fadeAmount;
       }
-    
+      
+      //Serial.print("Target: ");
+      //Serial.println(target);
+      
       analogWrite(led, (int)brightness);
-
-      Serial.print("Brightness: ");
-      Serial.println(brightness);
-      Serial.print("Target: ");
-      Serial.println(target);
-      /*Serial.println(i);
-      Serial.println(j);*/
-      Serial.println(" ");
     }
 
     brightness = target;
     analogWrite(led, (int)brightness);
   }
-  
-  Serial.print("Amplify: ");
-  Serial.println(amplify);
 
+  if(k <= amplify && millis() - outTimeMillis >= outTime)                               // Ausgabe der Verstärkung als Blinksignal
+  {
+    
+    if(millis() - lagMillis >= lag && millis() - lagMillis <= 2 * lag)
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else if(millis() - lagMillis >= 2 * lag)
+    {
+      lagMillis = millis();
+      digitalWrite(LED_BUILTIN, LOW);
+      k += 1;
+    }
+    
+  }
+  else if(k > amplify)
+  {
+    k = 0;
+    outTimeMillis = millis();
+  }
 
-  /*// Limiter
-  if(brightness >= pwm)
-  {
-    brightness = pwm;
-    i = i + 1;
-  }
-  else if(brightness <= pwmMin)
-  {
-    brightness = pwmMin;
-    j = j + 1;
-  }
-
-  analogWrite(led, (int)brightness);
-  
-
-  // Regulater
-  if(i > limit && amplify > amplifyMin)
-  {
-    amplify = amplify - difference;
-    i = 0;
-    j = 0;
-  }
-  else if(j > limit && amplify < amplifyMax)
-  {
-    amplify = amplify + difference;
-    j = 0;
-    i = 0;
-  }
-  
+  Serial.print("Brightness: ");
   Serial.println(brightness);
-  Serial.println(i);
-  Serial.println(j);
-  Serial.println(amplify);*/
+  //Serial.println(peakToPeak);
+  //Serial.print("Amplify: ");
+  //Serial.println(amplify);
+  //Serial.print("j: ");
+  //Serial.println(j);
+  //Serial.print("i: ");
+  //Serial.println(i);
+  Serial.println(" ");
+  
 }
